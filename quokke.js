b42d15c9332cfe5c7eb0e874fc9b96a2173c929d
oktio89 <wheeze20@gmail.com>
2022-07-25 23:28:00 +0200

    ejercicio js

--- quokke.js
+++ quokke.js
@@ -1,3 +1,22 @@
 console.log(
   "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo"
 );
+// null, undefined,false, 0 => falsy
+// null esta en memoria pero no esta asignado
+if (null) {
+  // si cumple
+} else {
+  // si no cumple
+}
+const yo = [
+  "antonio",
+  23,
+  true,
+  "9/15/99",
+  (book = {
+    titulo: "Los Cuatro Acuerdos",
+    autor: "Miguel Ángel Ruiz",
+    fecha: Date(1997),
+    url: "",
+  }),
+];
