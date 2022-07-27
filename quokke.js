c738dfded410dd52afe3a6f6c834d41d4fcf8137
oktio89 <wheeze20@gmail.com>
2022-07-27 04:08:52 +0200

    string

--- quokke.js
+++ quokke.js
@@ -32,3 +32,21 @@ for (let prop in persona) {
   console.log(prop);
   console.log(persona[prop]);
 }
+// ejercicio strings
+let nombre = "Antonio";
+let apellido = "Lopez";
+let estudiante = `Estudiante ${nombre} ${apellido}`;
+let estudianteMayus = `Estudiante ${nombre} ${apellido}`;
+let estudianteMinus = `Estudiante ${nombre} ${apellido}`;
+
+console.log(estudiante);
+console.log(estudianteMayus.toUpperCase());
+console.log(estudianteMinus.toLocaleLowerCase());
+console.log(estudiante.length);
+console.log(estudiante.indexOf("Antonio"));
+console.log(estudiante.match("Lopez"));
+
+console.log(estudiante.slice(11, 12));
+console.log(estudiante.slice(19, 20));
+console.log(estudiante.trim());
+console.log(estudiante.includes("Antonio"));
