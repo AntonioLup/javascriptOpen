3436748c7b646695ea0c056f6bf73c8f8fbe48c3
oktio89 <wheeze20@gmail.com>
2022-07-29 06:39:02 +0200

    arrays

--- quokke.js
+++ quokke.js
@@ -6,3 +6,4 @@ console.log(parseInt(altura));
 console.log(parseFloat(alturaM));
 console.log(Math.ceil(kg));
 console.log(Math.floor(kg));
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
