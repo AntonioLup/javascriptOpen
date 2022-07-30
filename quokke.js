71c9e625452348e8593fe25955595c0a03348513
oktio89 <wheeze20@gmail.com>
2022-07-30 23:53:21 +0200

    edad

--- quokke.js
+++ quokke.js
@@ -1,12 +1,2 @@
 // objeto sets
-const hoy = new Date();
-
-const nacimiento = new Date(1999, 8, 15);
-
-const comparacion = hoy > nacimiento;
-
-console.log(comparacion);
-
-const diaNacimiento = nacimiento.getDate();
-const mesNacimiento = nacimiento.getMonth() + 1;
-const anyoNacimiento = nacimiento.getFullYear();
+const edad = prompt("¿Cuántos años tienes?");
