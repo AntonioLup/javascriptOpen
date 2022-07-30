c7640a59cca5f1eeacfafae8a39c57bad8b5df9c
oktio89 <wheeze20@gmail.com>
2022-07-30 23:05:25 +0200

    set

--- quokke.js
+++ quokke.js
@@ -1,39 +1,6 @@
-let compra = ["leche", "carne", "huevos", "cereales", "nueces"];
-compra.push("Aceite de Girasol");
-console.log(compra);
-compra.pop();
-console.log(compra);
-// objeto
-const pelicula = [
-  (peli1 = {
-    tiulo: "El hombre de Toronto",
-    director: "Patrick Hughes",
-    fecha: 2009,
-  }),
-  (peli2 = {
-    tiulo: "Nobody",
-    director: "Ilya Naishuller",
-    fecha: 2021,
-  }),
-  (peli3 = {
-    tiulo: "Midsommar",
-    director: "Ari Aster",
-    fecha: 2019,
-  }),
-];
-pelicula.filter((e) => {
-  if (e.fecha > 2010) {
-    console.log(e);
-  }
-});
-const directore = pelicula.map((i) => {
-  console.log(i.director);
-  return i.director;
-});
-const titulos = pelicula.map((i) => {
-  console.log(i.tiulo);
-  return i.tiulo;
-});
-console.log(...directore, ...titulos);
+// objeto sets
+const familia = new Set(["batman", "punisher", "superman"]);
+familia.add("sr strange");
+familia.add("js");
 
-console.log(pelicula);
+console.log(familia);
