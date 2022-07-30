5b634fa6c36ceac7b4c4b6a59b0b2f9c579a10ea
oktio89 <wheeze20@gmail.com>
2022-07-30 23:12:47 +0200

    personal

--- quokke.js
+++ quokke.js
@@ -1,6 +1,31 @@
 // objeto sets
-const familia = new Set(["batman", "punisher", "superman"]);
-familia.add("sr strange");
-familia.add("js");
+const datosP = {
+  nombre: "Antonio",
+  apellido: "Lopez",
+  edad: 23,
+  altura: 1.72,
+  eresDesarrollador: true,
+};
+const { edad } = datosP;
+console.log(edad);
 
-console.log(familia);
+const datosMF = [
+  {
+    ...datosP,
+  },
+  {
+    nombre: "dani",
+    apellido: "gomez",
+    edad: 20,
+    altura: 1.82,
+    eresDesarrollador: false,
+  },
+  {
+    nombre: "jesus",
+    apellido: "contreras",
+    edad: 20,
+    altura: 1.68,
+    eresDesarrollador: false,
+  },
+];
+console.log(datosMF[2]);
