a8c14f54a3144ed7b2dcc154f9fa83c007931e0b
oktio89 <wheeze20@gmail.com>
2022-07-30 23:51:21 +0200

    fecha

--- quokke.js
+++ quokke.js
@@ -1,31 +1,12 @@
 // objeto sets
-const datosP = {
-  nombre: "Antonio",
-  apellido: "Lopez",
-  edad: 23,
-  altura: 1.72,
-  eresDesarrollador: true,
-};
-const { edad } = datosP;
-console.log(edad);
+const hoy = new Date();
 
-const datosMF = [
-  {
-    ...datosP,
-  },
-  {
-    nombre: "dani",
-    apellido: "gomez",
-    edad: 20,
-    altura: 1.82,
-    eresDesarrollador: false,
-  },
-  {
-    nombre: "jesus",
-    apellido: "contreras",
-    edad: 20,
-    altura: 1.68,
-    eresDesarrollador: false,
-  },
-];
-console.log(datosMF[2]);
+const nacimiento = new Date(1999, 8, 15);
+
+const comparacion = hoy > nacimiento;
+
+console.log(comparacion);
+
+const diaNacimiento = nacimiento.getDate();
+const mesNacimiento = nacimiento.getMonth() + 1;
+const anyoNacimiento = nacimiento.getFullYear();
