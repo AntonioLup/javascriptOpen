a8a5dac5ad13a36a738b899ef09e1c357423be8b
oktio89 <wheeze20@gmail.com>
2022-08-03 05:04:04 +0200

    log

--- quokke.js
+++ quokke.js
@@ -1,12 +1,33 @@
 // llamar
-async function llamar() {
-  return new Promise((res, rej) => {
-    res("Hola wey");
-  });
-}
+const winston = require("winston");
 
-llamar().then((res) => {
-  setTimeout(() => {
-    console.log(res);
-  }, 5000);
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
 });
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+try {
+  logger.info("ey que pasa");
+} catch (e) {
+  logger.error(e.message);
+}
