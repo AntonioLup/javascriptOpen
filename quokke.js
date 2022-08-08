8abf31c3444d680fbdd41b7fb66d4c0f9898238f
oktio89 <wheeze20@gmail.com>
2022-08-08 12:40:44 +0200

    poo

--- quokke.js
+++ quokke.js
@@ -1,27 +1,14 @@
-// llamar
-// const moduleMath = require("./module.js");
+class Estudiante {
+  nombre = "Antonio";
+  asignaturas = ["Javascript", "HTML", "CSS"];
 
-// const sumaFact = moduleMath.suma(5, 5);
+  obtenerDatos() {
+    return {
+      nombre: this.nombre,
+      asignaturas: this.asignaturas,
+    };
+  }
+}
 
-// const { suma } = require("./module.js");
-
-import { suma } from "./module.js";
-import multi from "./module.js";
-import chalk from "chalk";
-
-const sumaFact = suma(1, 2);
-const sumaFact2 = suma(4, 5);
-console.log(chalk.green(sumaFact));
-console.log(chalk.blue(sumaFact2));
-
-// import axios from "axios";
-// axios
-//   .get("https://pokeapi.co/api/v2/pokemon/ditto")
-//   .then(function (response) {
-//     // handle success
-//     console.log(response.data);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   });
+const estudiante = new Estudiante();
+console.log(estudiante.obtenerDatos());
