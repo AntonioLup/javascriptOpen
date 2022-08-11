c8d29830fc5448203bf12f70a833fa281096e73b
oktio89 <wheeze20@gmail.com>
2022-08-11 17:58:30 +0200

    lint

--- quokke.js
+++ quokke.js
@@ -1,11 +1,17 @@
-function Fibonacci(num) {
-  if (num === 1) return [1];
-  if (num === 2) return [1, 1];
-  let list = [1, 1];
-  for (let i = 2; i < num; i++) {
-    list.push(list[i - 1] + list[i - 2]);
-  }
-  return list;
-}
+let nombre = "Antonio";
+let apellido = "Lopez";
+let estudiante = `Estudiante ${nombre} ${apellido}`;
+let estudianteMayus = `Estudiante ${nombre} ${apellido}`;
+let estudianteMinus = `Estudiante ${nombre} ${apellido}`;
 
-console.log(Fibonacci(6));
+console.log(estudiante);
+console.log(estudianteMayus.toUpperCase());
+console.log(estudianteMinus.toLocaleLowerCase());
+console.log(estudiante.length);
+console.log(estudiante.indexOf("Antonio"));
+console.log(estudiante.match("Lopez"));
+
+console.log(estudiante.slice(11, 12));
+console.log(estudiante.slice(19, 20));
+const estudianteSinEspacios = estudiante.replace(/ /g, "");
+console.log(estudiante.includes("Antonio"));
