f644176625910684b92ac188aaac1638af10880c
oktio89 <wheeze20@gmail.com>
2022-08-13 20:33:39 +0200

    jq

--- module.js
+++ module.js
@@ -1,11 +1,18 @@
-export function suma(a, b) {
-  return a + b;
-}
+// const palabra = prompt("que digo?");
+// const color = prompt("color?");
 
-function multi(a, b) {
-  return a * b;
-}
-export default multi;
-// module.exports = {
-//   suma,
-// };
+// title.addEventListener("cambiarText", (e) => {
+//   console.log(e.detail);
+//   title.innerHTML = e.detail.text;
+//   title.style.color = e.detail.color;
+// });
+// function cambiarText(nuevoText, color) {
+//   const e = new CustomEvent("cambiarText", {
+//     detail: {
+//       text: nuevoText,
+//       color,
+//     },
+//   });
+//   title.dispatchEvent(e);
+// }
+// cambiarText(palabra, color);
--- quokke.js
+++ quokke.js
@@ -1,17 +1,12 @@
-let nombre = "Antonio";
-let apellido = "Lopez";
-let estudiante = `Estudiante ${nombre} ${apellido}`;
-let estudianteMayus = `Estudiante ${nombre} ${apellido}`;
-let estudianteMinus = `Estudiante ${nombre} ${apellido}`;
+console.clear();
 
-console.log(estudiante);
-console.log(estudianteMayus.toUpperCase());
-console.log(estudianteMinus.toLocaleLowerCase());
-console.log(estudiante.length);
-console.log(estudiante.indexOf("Antonio"));
-console.log(estudiante.match("Lopez"));
+const btn = document.getElementById("btn");
 
-console.log(estudiante.slice(11, 12));
-console.log(estudiante.slice(19, 20));
-const estudianteSinEspacios = estudiante.replace(/ /g, "");
-console.log(estudiante.includes("Antonio"));
+btn.addEventListener("click", () => {
+  alert("click en el boton");
+});
+$(() => {
+  $("#btnJQ").on("click", () => {
+    console.log("Estoy aquine");
+  });
+});
