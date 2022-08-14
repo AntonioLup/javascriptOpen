0331cce64e97bd424e51a1a1ae50f10891f172ca
oktio89 <wheeze20@gmail.com>
2022-08-14 22:11:42 +0200

    drop

--- quokke.js
+++ quokke.js
@@ -1,16 +1,43 @@
 console.clear();
 
-const nombre = "Antonio";
-const apellido = "Lopez";
+const ps = document.querySelectorAll(".prrafo");
+const sec = document.querySelectorAll(".seccion");
 
-const persona = {
-  nombre: nombre,
-  apellido: apellido,
-};
-localStorage.setItem("nombre", nombre);
-localStorage.setItem("apellido", apellido);
-localStorage.setItem("persona", JSON.stringify({ nombre, apellido }));
-console.log(JSON.parse(localStorage.getItem("persona")));
-console.log(localStorage.getItem("nombre"));
-const now = new Date();
-// document.cookie = `datos=${JSON.stringify(datos)};expires=${new Date(now.getTime() + 2 * 60000)}`
+ps.forEach((p) => {
+  p.addEventListener("dragstart", (e) => {
+    console.log("inicio");
+    p.classList.add("drag");
+    e.dataTransfer.setData("id", p.id);
+  });
+  p.addEventListener("dragend", () => {
+    console.log("fin");
+    p.classList.remove("drag");
+  });
+});
+
+sec.forEach((se) => {
+  se.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "move";
+
+    console.log("drag over");
+  });
+  se.addEventListener("drop", (e) => {
+    console.log("drop");
+    const id_parrafo = e.dataTransfer.getData("id");
+    console.log(id_parrafo);
+
+    const parrafo = document.getElementById(id_parrafo);
+    se.appendChild(parrafo);
+  });
+});
+const images = document.querySelector(".papelera");
+
+images.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  e.dataTransfer.dropEffect = "copy";
+});
+images.addEventListener("drop", (e) => {
+  const id_parrafo = e.dataTransfer.getData("id");
+  document.getElementById(id_parrafo).remove();
+});
