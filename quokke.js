3f4587f4271486a4704e360793fcc2d863e7ca40
oktio89 <wheeze20@gmail.com>
2022-08-15 18:34:36 +0200

    map

--- quokke.js
+++ quokke.js
@@ -1,43 +1,37 @@
 console.clear();
 
-const ps = document.querySelectorAll(".prrafo");
-const sec = document.querySelectorAll(".seccion");
-
-ps.forEach((p) => {
-  p.addEventListener("dragstart", (e) => {
-    console.log("inicio");
-    p.classList.add("drag");
-    e.dataTransfer.setData("id", p.id);
+let marker, map;
+function initMap() {
+  console.log("inicio");
+  const posicion = {
+    lat: -25.363,
+    lng: 131.044,
+  };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: posicion,
   });
-  p.addEventListener("dragend", () => {
-    console.log("fin");
-    p.classList.remove("drag");
+  const marker = new google.maps.Marker({
+    posicion: posicion,
+    map,
+    title: "Estas aqui",
   });
-});
-
-sec.forEach((se) => {
-  se.addEventListener("dragover", (e) => {
-    e.preventDefault();
-    e.dataTransfer.dropEffect = "move";
-
-    console.log("drag over");
-  });
-  se.addEventListener("drop", (e) => {
-    console.log("drop");
-    const id_parrafo = e.dataTransfer.getData("id");
-    console.log(id_parrafo);
-
-    const parrafo = document.getElementById(id_parrafo);
-    se.appendChild(parrafo);
-  });
-});
-const images = document.querySelector(".papelera");
-
-images.addEventListener("dragover", (e) => {
-  e.preventDefault();
-  e.dataTransfer.dropEffect = "copy";
-});
-images.addEventListener("drop", (e) => {
-  const id_parrafo = e.dataTransfer.getData("id");
-  document.getElementById(id_parrafo).remove();
-});
+}
+function getPos() {
+  if (navigator.geolocation) {
+    const geoLoc = navigator.geolocation;
+    const watchPos = geoLoc.watchPosition(
+      centraMapa,
+      (error) => console.log(error),
+      { timeout: 60000 }
+    );
+  } else {
+    alert("tu navegador no admite esto");
+  }
+}
+function centraMapa(pos) {
+  const nuevaPos = {
+    lat: pos.coords.latitud,
+    lng: pos.coords.longitude,
+  };
+}
